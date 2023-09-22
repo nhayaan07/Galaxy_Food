9200d8ceb4000828eccee6b1555cbb02a0dd3133
Nazmul <najmuljs7@gmail.com>
2023-09-22 23:28:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+
+
+<div className='flex w-screen bg-green-300 h-screen items-center justify-center box-border'>
+<h1 className= "text-2xl p-2 text-black font-bold">Hello , This is Nazmul <br />
+Testing and deploying to Github
+</h1>
+</div>
+
+
+
+
+  )
+}
+
+export default App
