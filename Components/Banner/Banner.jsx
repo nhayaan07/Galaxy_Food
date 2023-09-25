5cc70f537b236c2f7d56fba2978351f9e6f5c59b
Nazmul <najmuljs7@gmail.com>
2023-09-26 00:56:25 +0600

    Add but nothing special

--- Components/Banner/Banner.jsx
+++ Components/Banner/Banner.jsx
@@ -21,7 +21,7 @@ const Banner = () => {
 <div className='flex flex-col space-y-8 items-start h-full justify-start p-5 sm:items-center md:justify-center max-md:pt-[60%]'>
   <h1 data-aos="fade-up" className='text-5xl md:text-7xl text-white'>কাচ্চির <span className='text-[--secondary-color]'>ভুবনে</span> আপনাকে স্বাগতম</h1>
   <p data-aos="zoom-in" data-aos-delay="500" className='text-white text-2xl'>শহরের সেরা কাচ্চির স্বাদ পেতে আপনাকে আসতে হবে গ্যালাক্সি ফুডে</p>
-  <button data-aos="fade" data-aos-delay="700" className='btn-default bg-[--secondary-color] font-semibold'>আমাদের মেনু দেখুন</button>
+  <a href="#menu"><button data-aos="fade" data-aos-delay="700" className='btn-default bg-[--secondary-color] font-semibold'>আমাদের মেনু দেখুন</button></a>
 </div>
 
 </div>
--- Components/Menu/Menu.jsx
+++ Components/Menu/Menu.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./Menu.css"
 
 const Menu = () => {
+
+const tabTitle = useRef(null);
+const [activeTab, setActiveTab] = useState(0);
+
+const handleTabClick = (index) => {
+  setActiveTab(index);
+};
+useEffect( 
+  () => {
+console.log(tabTitle.current.querySelectorAll('li'))
+
+  }
+  ,[])
   return (
-    <div>Menu</div>
+    <>
+    
+    <div className=' overflow-hidden' id='menu'>
+<div>
+  <h1 className='tab-title text-6xl text-center py-8'>আমাদের স্পেশাল মেনু</h1>
+<div className=''>
+  <ul ref={tabTitle} className='tab-head flex text-2xl md:text-4xl gap-10 justify-center flex-wrap'>
+    <li>কাচ্চি</li>
+    <li>কাচ্চি</li>
+
+    <li>কাচ্চি</li>
+
+    <li>কাচ্চি</li>
+    <li>কাচ্চি</li>
+
+    <li>কাচ্চি</li>
+
+  </ul>
+</div>
+
+<div className='tab-panel p-5'>
+
+
+<div className="tab-content active">
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+
+</div>
+<div className="tab-content">
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+
+</div>
+<div className="tab-content">
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+<h1 className='text-6xl py-8'>আমাদের স্পেশাল মেনু</h1>
+
+</div>
+
+
+
+
+</div> 
+{/* tab panel div end */}
+
+</div>
+
+
+    </div>
+    
+    
+    </>
   )
 }
 
